541161a02bd7533e4053a60502e6c30bd0259caf
Katia <katerina.bogrom@gmail.com>
2021-07-14 16:30:23 +0200

    initial commit

---
+++ src/dosVariables/Actividad1App.java
@@ -0,0 +1,28 @@
+package dosVariables;
+
+public class Actividad1App {
+
+	public static void main(String[] args) {
+
+		// declaración de las variables,
+		int number1 = 34;
+		double number2 = -123.56;
+
+		// suma,
+		System.out.println("Suma de " + number1 + " y " + number2 + " es: " + (number1 + number2));
+
+		// resta,
+		System.out.println("Resta de " + number1 + " y " + number2 + " es: " + (number1 - number2));
+
+		// multiplicación,
+		System.out.println("Multiplicación " + number1 + " por " + number2 + " es: " + (number1 * number2));
+
+		// división,
+		System.out.println("División " + number1 + " entre " + number2 + " es: " + (number1 / number2));
+
+		// módulo,
+		System.out.println("Módulo de " + number2 + " entre " + number1 + " es: " + (number2 % number1));
+
+	}
+
+}
